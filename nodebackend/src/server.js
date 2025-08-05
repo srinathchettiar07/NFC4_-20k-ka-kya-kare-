48d1baf29a5a2291779dffe9bc7ef7aade3a6db6
srinathchettiar07 <srinathchettiaar786@gmail.com>
2025-08-05 12:29:14 +0530

    Backend completed

---
+++ nodebackend/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { connectMongoDB } from "./lib/Database/MongoDB.js";
+import {connectPgDB} from "./lib/Database/Postgres.js";
+config();
+const app = express();
+
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+}))
+const port = process.env.PORT;
+
+app.use("/api/auth" , authRoutes) ;
+// app.use("api/users/admin" , StudentRoutes );
+// app.use("api/users/teacher" , StudentRoutes);
+
+
+app.listen(port , ()=>
+{
+    console.log("Server running on localhost : "+port);
+    connectMongoDB();
+    connectPgDB();
+})
