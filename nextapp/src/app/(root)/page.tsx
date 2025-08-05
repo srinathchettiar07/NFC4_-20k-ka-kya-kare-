6b01e128922e68da20f25cc55526b7791899ff77
Shafiq <alphaaas10@gmail.com>
2025-08-05 12:45:25 +0530

    next routing 1

---
+++ nextapp/src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import WrapButton from "@/components/ui/wrap-button";
+
+export default function Home() {
+  return (
+    <div>
+      hello world
+      <WrapButton>
+        Get Started
+      </WrapButton>
+    </div>
+  );
+}
